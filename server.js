cdcfbc2b25e17f64bdfa8ee6cab2f1278ac045fd
Muhammet <nsaryyev9797@gmail.com>
2021-08-06 17:27:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+server.listen(3000);
